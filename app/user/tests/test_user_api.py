c257390b5c3e840b319b22d26fb1cce6952e2de5
chatsai <ctsai_mail@yahoo.com>
2020-07-05 12:04:53 -0500

    Endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -65,6 +65,7 @@ class PublicUserApiTests(TestCase):
         create_user(**payload)
         res = self.client.post(TOKEN_URL, payload)
 
+
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
